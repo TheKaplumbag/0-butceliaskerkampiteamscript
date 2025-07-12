eb442a8058cc4e7dc87fc173e1c7606822975c5d
TheKaplumbag <scpmtf4@gmail.com>
2025-07-12 05:21:33 +0300

    Update BTF kuyumcu soygun.lua

--- BTF kuyumcu soygun.lua
+++ BTF kuyumcu soygun.lua
@@ -7,47 +7,47 @@ local humrp = c.HumanoidRootPart
 
 --- functionlar ---
 local function proxfire()
-for _, v in pairs(Folder:GetChildren()) do
-if v:IsA("Model") then
-for _, r in pairs(v:GetChildren()) do
-if r.Name == "Ring" then
-for _, r2 in pairs(r:GetChildren()) do
-if r2.Name == "Ring" then
-for _, c in pairs(r2:GetChildren()) do
-if c.Name == "Prompt" then
-if c.Enabled then
-fireproximityprompt(c)
-else
-print("Not enabled")
-end
-end
-end
-end
-end
-end
-end
-end
-end
+  for _, v in pairs(Folder:GetChildren()) do
+    if v:IsA("Model") then
+      for _, r in pairs(v:GetChildren()) do
+        if r.Name == "Ring" then
+          for _, r2 in pairs(r:GetChildren()) do
+            if r2.Name == "Ring" then
+              for _, c in pairs(r2:GetChildren()) do
+                if c.Name == "Prompt" then
+                  if c.Enabled then
+                    fireproximityprompt(c)
+                  else
+                    print("Not enabled")
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
 local function remover()
-for _, v in pairs(Folder:GetChildren()) do
-if v:IsA("Model") then
-for _, c in pairs(v:GetChildren()) do
-if c.Name == "JewelBox" then
-c:Destroy()
-end
-end
-end
-end
+  for _, v in pairs(Folder:GetChildren()) do
+    if v:IsA("Model") then
+      for _, c in pairs(v:GetChildren()) do
+        if c.Name == "JewelBox" then
+          c:Destroy()
+        end
+      end
+    end
+  end
 end
 
 local function Main()
-humrp.CFrame = Floor.CFrame + Vector3.new(0, 10, 0)
-task.wait(0.1)
-remover()
-task.wait(0.5)
-proxfire()
+  humrp.CFrame = Floor.CFrame + Vector3.new(0, 10, 0)
+  task.wait(0.1)
+  remover()
+  task.wait(0.5)
+  proxfire()
 end
 --- Baslatici ---
 Main()
